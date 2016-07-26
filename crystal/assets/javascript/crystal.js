bf5717c0d8ed8ea35841114c26c56d86b94d031a
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-07-26 00:09:42 -0700

    fixed bug where the count doesn't reset once a new game starts

--- crystal/assets/javascript/crystal.js
+++ crystal/assets/javascript/crystal.js
@@ -12,8 +12,7 @@ $(document).ready(function(){
 	// gem3 = randomize();
 	// gem4 = randomize();
 
-var gem1, gem2, gem3, gem4, randomNumber;
-var userTotal = 0;
+var gem1, gem2, gem3, gem4, randomNumber, userTotal;
 var wins = 0;
 var losses = 0;
 
@@ -25,16 +24,14 @@ function start(){
 	gem3 = Math.floor(Math.random() * 12) + 1;
 	gem4 = Math.floor(Math.random() * 12) + 1;
 	randomNumber = Math.floor(Math.random() * 101) + 19;
-	
+	userTotal = 0;
+
 	console.log('#1 ' + gem1);
 	console.log('#2 ' + gem2);
 	console.log('#3 ' + gem3);
 	console.log('#4 ' + gem4);
 	console.log('WINS: ' + wins + ' LOSSES: ' + losses);
-	
 	console.log('Match the number: ' + randomNumber);
-	
-	var userTotal = gem1 + gem2 + gem3 + gem4;
 
 }	
 
